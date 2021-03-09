cc175322e08f5706419ef29d782d630b53157ff0
yvonneeeee <yvonnelin840701@gmail.com>
2021-03-09 02:18:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: Hard-coding 'cat' below will not work
-  return animal
+  return myAnimal()
 }
 
 function add2(n) {
